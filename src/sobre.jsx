4ba61ef1c858aac7e7b46405be97345902b2a1d9
Tiago Santana Palmeira <125421932+Tiagopalmeira@users.noreply.github.com>
2024-06-17 11:20:24 -0300

    Update sobre.jsx

--- src/sobre.jsx
+++ src/sobre.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import './CSS/sobremim.css';
 
 export default function SobreMim() {
-    return (
-        
+   return (
+
         <div>
             <div className="galinho">
 
@@ -18,6 +18,11 @@ export default function SobreMim() {
                 <div className="card">
                     <h4> Plano básico </h4>
                     <p>Acompanhe os seus desbravadores com facilidade por um preço acessivel.</p>
+                    <ul className="listabeneficios">
+                        <li>Cadastre e acompanhe membros cadastrados</li>
+                        <li>Atualize classes e acompanhe as classes desenvolvidas</li>
+                        <li>Tenha acesso a mensagens personalizadas para lembretes</li>
+                    </ul>
                     <span className="preco"> R$ X,99 </span> <span>/mensal</span> <br />
                     <button className="contratar">Contratar</button>
                 </div>
@@ -25,6 +30,14 @@ export default function SobreMim() {
                 <div className="card">
                     <h4> Plano premium </h4>
                     <p>Cadastre classes, atualize e acompanhe seus desbravadores de uma maneira geral. Clube organizado é clube vencedor.</p>
+                    <ul className="listabeneficios">
+                        <li>Cadastre e acompanhe membros cadastrados</li>
+                        <li>Atualize classes e acompanhe as classes desenvolvidas</li>
+                        <li>Tenha acesso a mensagens personalizadas para lembretes</li>
+                        <li>Tenha acesso a todos os cartões de classe atualizado para download</li>
+                        <li>Cadastre os membros de todas as unidades e acompanhe de forma geral o desenvolvimento</li>
+                        <li>Acompanhe e monitore o andamento de cada membro na execução de suas atividades</li>
+                    </ul>
                     <span className="preco"> R$ X,99 </span> <span> /mensal </span><br />
                     <button className="contratar">Contratar</button>
                 </div>
